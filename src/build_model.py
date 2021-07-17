bf48f0a4466a65fc0304469e4c87972c8fc3993b
Oleg Kryachun <55558145+okryachun@users.noreply.github.com>
2021-07-16 17:34:07 -0700

    Add facenet transfer learned model.

--- src/build_model.py
+++ src/build_model.py
@@ -8,12 +8,15 @@ Created on Thu Jun 17 11:27:10 2021
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import (Input, Conv2D, Dense,
                                      BatchNormalization,
-                                     Flatten, Dropout)
+                                     Dropout, GlobalAveragePooling2D)
+
+# local imports
+from run_model import get_model
 
 class ImageModel():
     """Builds a multi-branched model for multiple outputs."""
 
-    def head_branch(self, inputs):
+    def head_layer(self, inputs):
         """Initial convolution layers to each branch.
         
         Parameters
@@ -23,87 +26,131 @@ class ImageModel():
         -------
             x (Conv2D): last layer of the convolution chain
         """
-        x = Conv2D(16, (3,3), strides=2, padding="same", activation="relu")(inputs)
+        x = Conv2D(16, (3,3), strides=1, padding="same", activation="relu")(inputs)
+        x = BatchNormalization(axis=-1)(x)
+        x = Conv2D(16, (3,3), strides=2, padding="same", activation="relu")(x)
+        x = BatchNormalization(axis=-1)(x)
+        x = Conv2D(32, (3,3), strides=1, padding="same", activation="relu")(x)
         x = BatchNormalization(axis=-1)(x)
         x = Conv2D(32, (3,3), strides=2, padding="same", activation="relu")(x)
         x = BatchNormalization(axis=-1)(x)
+        x = Conv2D(32, (3,3), strides=1, padding="same", activation="relu")(x)
+        x = BatchNormalization(axis=-1)(x)
         x = Conv2D(32, (3,3), strides=2, padding="same", activation="relu")(x)
         x = BatchNormalization(axis=-1)(x)
-        x = Conv2D(64, (3,3), strides=2, padding="same", activation="relu")(x)
+        x = Conv2D(64, (3,3), strides=1, padding="same", activation="relu")(x)
         x = BatchNormalization(axis=-1)(x)
         x = Conv2D(64, (3,3), strides=2, padding="same", activation="relu")(x)
+        x = BatchNormalization(axis=-1)(x)
+        x = Conv2D(64, (3,3), strides=1, padding="same", activation="relu")(x)
+        x = BatchNormalization(axis=-1)(x)
+        x = Conv2D(64, (3,3), strides=2, padding="same", activation="relu")(x)
+        x = BatchNormalization(axis=-1)(x)
+        x = Conv2D(128, (3,3), strides=1, padding="same", activation="relu")(x)
+        x = BatchNormalization(axis=-1)(x)
+        x = Conv2D(128, (3,3), strides=1, padding="same", activation="relu")(x)
         x = BatchNormalization(axis=-1)(x)
         x = Conv2D(128, (3,3), strides=2, padding="same", activation="relu")(x)
-
+        
         return x
+           
+    def facenet_head(self, inputs):
+        """Load facenet model, exlude last few layers after "add_41"
 
-    def category_branch(self, inputs, n_categories, name):
+        Parameters
+        ----------
+            inputs (Input): Input layer from tensorflow.keras.layer.Input
+        Returns
+        -------
+            facenet_model (Model): facenet model with trimmed bottleneck layers
+        """
+        facenet_model = get_model("saved_models/facenet_transfer_layer")
+
+        # get layer before bottlneck
+        facenet_model = Model(facenet_model.input,
+                       outputs=facenet_model.get_layer("add_41").output,
+                       name='facenet_transfer_layer')
+
+        # Set last 20 layers to trainable
+        facenet_model.trainable = False
+        for layer in facenet_model.layers[-20:]:
+            layer.trainable = True
+            
+        # convert model to sequential layer
+        facenet_model = facenet_model(inputs )
+        
+        return facenet_model
+
+    def category_branch(self, head_layer, n_categories, name):
         """Categorical tail end of CNN branch for gender & race.
         
         Parameters
         ----------
-            inputs (Input): Input layer from tensorflow.keras.layer.Input
+            facenet_model (Model): trained facenet model
             n_categories (int): number of outputs for categerical branch
             name (str): name of branch output
         Returns
         -------
             x (Dense): layer that outputs final prediction of branch
         """
-
-        x = self.head_branch(inputs)
-
-        x = Flatten()(x)
-        x = Dense(128, activation="relu")(x)
+        x = GlobalAveragePooling2D()(head_layer)
+        x = Dense(512, activation="relu")(x)
         x = BatchNormalization()(x)
-        x = Dropout(0.5)(x)
+        x = Dropout(0.3)(x)
+        x = Dense(512, activation="relu")(x)
+        x = BatchNormalization()(x)
+        x = Dropout(0.3)(x)
         x = Dense(n_categories, activation="sigmoid", name=name)(x)
 
         return x
 
-    def regression_branch(self, inputs, name):
+    def regression_branch(self, head_layer, name):
         """Regression tail end of CNN branch for age
         
         Parameters
         ----------
-            inputs (Input): Input layer from tensorflow.keras.layer.Input
+            facenet_model (Model): trained facenet model
             name (str): name of branch output
         Returns
         -------
             x (Dense): layer that outputs final prediction of branch
         """
-
-        x = self.head_branch(inputs)
-
-        x = Flatten()(x)
-        x = Dense(128, activation="relu")(x)
+        x = GlobalAveragePooling2D()(head_layer)
+        x = Dense(512, activation="relu")(x)
         x = BatchNormalization()(x)
-        x = Dropout(0.5)(x)
+        x = Dropout(0.3)(x)
+        x = Dense(512, activation="relu")(x)
+        x = BatchNormalization()(x)
+        x = Dropout(0.3)(x)
         x = Dense(1, name=name)(x)
 
         return x
 
-    def assemble_model(self, width, height, race_count, gender_count):
+    def assemble_model(self, model_type, input_shape, race_count, gender_count):
         """Assemble complete model of all branches for CNN
         
         Parameters
         ----------
-            width (int): width of input images
-            height (int): height of input images
+            model_type (str): name of model type to build ('facenet' or 'normal')
+            input_shape (list): input shape of image for CNN model
             race_count (int): categories of race
             gender_count (int): categories of gender
         Returns
         -------
             model (Model): fully assembled model of all layers & branches
         """
-        input_shape = (width, height, 3)
-        
+        if model_type == 'facenet':
+            head_layer = self.facenet_head
+        elif model_type == 'normal':
+            head_layer = self.head_layer
+
         inputs = Input(shape=input_shape)
-        
-        age_branch = self.regression_branch(inputs, "age_out")
-        gender_branch = self.category_branch(inputs,
-                                             gender_count, "gender_out")
-        race_branch = self.category_branch(inputs,
-                                           race_count, "race_out")
+    
+        head = head_layer(inputs)
+
+        age_branch = self.regression_branch(head, "age_out")
+        gender_branch = self.category_branch(head, gender_count, "gender_out")
+        race_branch = self.category_branch(head, race_count, "race_out")
 
         model = Model(inputs, outputs=[age_branch, race_branch, gender_branch],
                       name="face_model")
--- src/main.py
+++ src/main.py
@@ -22,10 +22,14 @@ if __name__ == "__main__":
     subparser = parser.add_subparsers()
     
     run_parser = subparser.add_parser("run", help='Run face analysis model.')
-    run_parser.add_argument('--model_path', nargs=1, default='model/saved_model',
-                            dest='model_path',
-                            help="path of saved trained model")
-    
+    run_parser.add_argument('--model_path', nargs=1,
+                            default='saved_models/facenet_model',
+                            dest='model_path', help="path of saved trained model")
+    run_parser.add_argument('--model_type', nargs='?', default='facenet',
+                            dest='model_type', const='facenet',
+                            choices=['facenet', 'normal'],
+                            help="Choose between two models: 'facenet' (transfer"
+                                " learned) or 'normal' (trained from scratch).")
     
     group = run_parser.add_mutually_exclusive_group(required=True)
     group.add_argument('--video', action='store_true',
@@ -38,11 +42,17 @@ if __name__ == "__main__":
     train_parse = subparser.add_parser('train', help='Train CNN model')
     train_parse.add_argument('--eval', action='store_true', dest='evaluate', 
                              help='Evaluate model training statistics.')
-    train_parse.add_argument('--model_path', nargs='?', default='model/saved_model',
+    train_parse.add_argument('--model_path', nargs='?',
+                             default='saved_models/facenet_model',
                              help='location to save trained model')
     train_parse.add_argument('--data_path', nargs='?', dest='data_path',
-                            default='face_data', 
+                            default='dataset', 
                             help="Path to data to train model.")
+    train_parse.add_argument('--model_type', nargs='?', default='facenet',
+                             const='facenet', dest='model_type',
+                             choices=['facenet', 'normal'],
+                             help="Choose between two models: 'facenet' (transfer"
+                                " learned) or 'normal' (trained from scratch).")
     train_parse.set_defaults(function=train_model)
     
     
